7949360f7b444acbb6859476ce03495fd4f830a3
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-24 09:48:10 -0700

    Updated Startcode to reflect Get request for both all and getByID

--- src/main/java/com/dcc/videoGameApi/controllers/VideoGameController.java
+++ src/main/java/com/dcc/videoGameApi/controllers/VideoGameController.java
@@ -23,4 +23,10 @@ public class VideoGameController {
         return service.GetCountOfGames();
     }
 
+    @GetMapping("/all")
+    public List<VideoGame> GetAll(){return service.GetAll();}
+
+    @GetMapping("/getById/{id}")
+    public VideoGame GetByID(@PathVariable Integer id) {return service.GetByID(id);}
+
 }
--- src/main/java/com/dcc/videoGameApi/service/VideoGameService.java
+++ src/main/java/com/dcc/videoGameApi/service/VideoGameService.java
@@ -19,6 +19,8 @@ public class VideoGameService {
     public long GetCountOfGames(){
         return videoGameRepository.count();
     }
+    public List<VideoGame> GetAll(){return videoGameRepository.findAll();}
 
+    public VideoGame GetByID(Integer id){return videoGameRepository.findById(id).orElse(null);}
 
 }
